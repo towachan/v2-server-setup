a7699ed4818e3d434614d739baee962f42e01183
towachan <ysjopk@gmail.com>
2022-05-07 11:13:37 +0800

    feat: add kcp script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # v2-server-setup
+
+## v2 setup - simple kcp
+bash <(curl -L https://raw.githubusercontent.com/towachan/v2-server-setup/main/towa-v2-setup.sh)
---
+++ towa-v2-setup.sh
@@ -0,0 +1,32 @@
+#/bin/sh
+
+port=$1
+client_id=$2
+
+v2_config_file="v2-kcp-config.json"
+raw_github_url="https://raw.githubusercontent.com/towachan/v2-server-setup/main"
+
+echo "Download and install v2ray..."
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+
+echo "Update geoip and geosite..."
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
+
+echo "Download v2ray config file..."
+curl -L $raw_github_url/$v2_config_file -o ~/$v2_config_file
+
+echo "Update v2ray config file..."
+sed -i '<PORT>' $1 ~/v2-kcp-config.json
+sed -i '<CLIENT_ID>' $1 ~/v2-kcp-config.json
+
+echo "Copy v2ray config file to working path..."
+cp ~/$v2_config_file /usr/local/etc/v2ray/config.json
+
+echo "Open $port in firewall"
+filewall-cmd --add-port=$port/udp --zone=public --permanent
+systemctl reload firewalld.service
+firewall-cmd --list-ports
+
+echo "Start v2ray service"
+systemctl start v2ray
+systemctl status v2ray
