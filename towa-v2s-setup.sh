c97e8cd4816378af77a75daaead0e6f21ee6a1e3
towachan <ysjopk@gmail.com>
2024-05-28 20:55:57 +0800

    chore: update DNS to CF

--- towa-v2s-setup.sh
+++ towa-v2s-setup.sh
@@ -6,8 +6,7 @@ path=$3
 nginx_port=$4
 server_name=$5
 email=$6
-gd_key=$7
-gd_secret=$8
+cf_key=$7
 
 v2_config_file="v2-ws-config.json.j2"
 nginx_config_file="nginx.conf.j2"
@@ -48,14 +47,12 @@ yum -y install socat
 yum -y install tar
 curl  https://get.acme.sh | sh -s email=$email
 
-echo "======================Register acme======================"
-~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-
 echo "======================Create ssl cert======================"
-export GD_Key=$gd_key
-export GD_Secret=$gd_secret
-# issue on CentOS 7 - https://github.com/acmesh-official/acme.sh/issues/3808
-~/.acme.sh/acme.sh --issue -d $server_name --dns dns_gd --keylength ec-256 --force
+
+export CF_Key=$cf_key
+export CF_Email=$email
+
+~/.acme.sh/acme.sh --issue -d $server_name --dns dns_cf --keylength ec-256 --force
 ~/.acme.sh/acme.sh --installcert -d $server_name --ecc \
     --fullchain-file $nginx_crt_file_path \
     --key-file $nginx_crt_key_path
@@ -81,35 +78,20 @@ echo "<!DOCTYPE html><html><body><p>I am a test page.</p></body></html>" > /web/
 echo "======================Copy v2ray nginx file to working path======================"
 cp ~/$nginx_config_file /etc/nginx/nginx.conf
 
-# echo "======================Set SELINUX premissive======================"
-# setenforce 0
-# sed -i "s/SELINUX=disabled/SELINUX=permissive/g" /etc/selinux/config
+echo "======================Set SELINUX premissive======================"
+setenforce 0
+sed -i "s/SELINUX=disabled/SELINUX=permissive/g" /etc/selinux/config
+sed -i "s/SELINUX=enforcing/SELINUX=permissive/g" /etc/selinux/config
 
-# echo "======================Open $nginx_port in firewall======================"
-# firewall-offline-cmd --add-port=28453/tcp --zone=public
-# firewall-cmd --add-port=$nginx_port/tcp --zone=public --permanent
-# systemctl reload firewalld.service
-# firewall-cmd --list-all
-
-
-echo "======================Install CF WARP======================"
-rpm -ivh https://pkg.cloudflareclient.com/cloudflare-release-el8.rpm
-yum -y install cloudflare-warp
+echo "======================Open $nginx_port in firewall======================"
+firewall-cmd --add-port=$nginx_port/tcp --zone=public --permanent
+systemctl reload firewalld.service
+firewall-cmd --list-all
 
 echo "======================Extend FD limit======================"
 sed -i "s/\[Service\]/\[Service\]\nLimitNOFILE=32768/g" "$systemd_folder/warp-svc.service"
 echo -e "\nLimitNOFILE=32768" >> "$systemd_folder/v2ray.service.d/*.conf"
 systemctl daemon-reload
-
-echo "======================Connect CF WARP======================"
-systemctl restart warp-svc
-systemctl | grep warp-svc
-warp-cli register
-warp-cli set-mode proxy
-warp-cli connect
-warp-cli enable-always-on
-export ALL_PROXY=socks5://127.0.0.1:40000
-curl ifconfig.me
 
 echo "======================Start v2ray service======================"
 systemctl start v2ray
